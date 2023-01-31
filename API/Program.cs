97aeb025570f6760e93b9476a84afea9d127563a
Brent Lawson <brent.lawson@fleetcor.com>
2023-01-30 19:22:36 -0500

    add login and token service

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,20 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+
 
 var app = builder.Build();
 
