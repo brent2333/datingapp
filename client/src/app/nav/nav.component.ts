10b6ce538e0398588306405ba3dbe8d2af43843d
Brent Lawson <brent.lawson@fleetcor.com>
2023-02-03 06:57:19 -0500

    finish register

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,25 +10,24 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn = false;
-  constructor(private accountService: AccountService) { }
+  // currentUser$: Observable<User | null> = of(null);
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-
+    // this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
