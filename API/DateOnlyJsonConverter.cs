b255ee976bb512e9c19ea9601982afffd561d55f
Brent Lawson <brent.lawson@fleetcor.com>
2023-02-07 07:37:44 -0500

    routing & errors in client, start db seeding

---
+++ API/DateOnlyJsonConverter.cs
@@ -0,0 +1,20 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace API
+{
+public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    private const string Format = "yyyy-MM-dd";
+
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.ParseExact(reader.GetString()!, Format, System.Globalization.CultureInfo.InvariantCulture);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(Format, System.Globalization.CultureInfo.InvariantCulture));
+    }
+}
+}
