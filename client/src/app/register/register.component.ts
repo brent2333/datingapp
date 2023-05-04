724b89a19f4d73fe7896f651b00aa02b65a7bb8f
Brent Lawson <brent.lawson@fleetcor.com>
2023-05-04 08:11:10 -0400

    start reactive form chgs

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,29 +10,52 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup = new FormGroup({});
 
   model: any = {};
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.intializeForm();
+  }
+
+  intializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello',Validators.required),
+      password: new FormControl('',[
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: error => {
-        console.log(error)
-        const errs = error.error.errors;
-        if (errs) {
-          const key = Object.keys(errs)[0];
-          this.toastr.error(errs[key] ? errs[key] : 'bad request')
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: error => {
+    //     console.log(error)
+    //     const errs = error.error.errors;
+    //     if (errs) {
+    //       const key = Object.keys(errs)[0];
+    //       this.toastr.error(errs[key] ? errs[key] : 'bad request')
 
-        }
-      }
-    })
+    //     }
+    //   }
+    // })
+    console.log(this.registerForm?.value);
   }
 
    cancel() {
